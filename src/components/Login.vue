ab71244f045f21ae03e28414242e1b80083f1aba
SpringOnU <760604648@qq.com>
2021-02-03 22:45:07 +0800

    login.4

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -27,12 +27,12 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       // 这是登录表单的数据绑定对象
       loginForm: {
-        username: 'zx',
-        password: '1234'
+        username: 'admin',
+        password: '123456'
       },
       // 这是表单的验证规则对象
       loginFormRules: {
@@ -52,24 +52,30 @@ export default {
   },
   methods: {
     // 点击重置按钮 重置登录表单
-    resetLoginForm () {
+    resetLoginForm() {
       // console.log(this) 可以引用访问 $refs
       this.$refs.loginFormRef.resetFields()
       // 调用resetFields函数代码
     },
-    login () {
+    login() {
       this.$refs.loginFormRef.validate(async valid => { // 随便起一个名字就可以 valid只是代表了这个布尔值 如果输入的用户名和密码都合法 就打印出true 不合法就打印出false 和valid没多大关系
-        // console.log(valid)
+      // validate() => 参数为一个回调函数。该回调函数会在校验结束后被调用，并传入两个参数：是否校验成功和未通过校验的字段。若不传入回调函数，则会返回一个 promise
+      // Function(callback: Function(boolean, object))
+        // console.log(valid) 这里就会返回 true /false
+        if (!valid) return
         const { data: res } = await this.$http.post('login', this.loginForm)
         // console.log(result)
         if (res.meta.status !== 200) return this.$message.error('登陆失败')
         this.$message.success('登陆成功')
+        // console.log(res)
+        //   1. 将登录成功之后的 token，保存到客户端的 sessionStorage 中
+        //     1.1 项目中除了登录之外的其他API接口，必须在登录之后才能访问
+        //     1.2 token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStorage 中
+        window.sessionStorage.setItem('token', res.data.token)
+        // 2. 通过编程式导航跳转到后台主页，路由地址是 /home
+        this.$router.push('/home')
+        // router跳转 home页面
       })
-      // validate() => 参数为一个回调函数。该回调函数会在校验结束后被调用，并传入两个参数：是否校验成功和未通过校验的字段。若不传入回调函数，则会返回一个 promise
-      // Function(callback: Function(boolean, object))
-      //   1. 将登录成功之后的 token，保存到客户端的 sessionStorage 中
-      //     1.1 项目中除了登录之外的其他API接口，必须在登录之后才能访问
-      //     1.2 token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStorage 中
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/Home.vue' // 添加路由规则
 Vue.use(Router)
-export default new Router({
+const router = new Router({
   routes: [
     { path: '/', redirect: '/login' },
-    { path: '/login', component: Login }
+    { path: '/login', component: Login },
+    { path: '/home', component: Home }
   ]
 })
+
+// 挂载路由导航守卫
+// 看进入home页面以后如果没有token，就强制跳转回login页面
+router.beforeEach((to, from, next) => {
+  // to 将要访问的函数
+// from 代表从哪个路径跳转而来
+// next 代表一个函数 表示放行
+//   next( ) 放行  next('/login') 强制跳转到相应界面
+  if (to.path === '/login') return next() // 用户要访问登录页面,直接放行
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  // 判断token
+  if (!tokenStr) return next('/login')
+  next()
+})
+
+export default router // 暴露路由对象
