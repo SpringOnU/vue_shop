3e6504d3772cdff23c321ed68a56305e2de770f5
SpringOnU <760604648@qq.com>
2021-02-04 22:45:04 +0800

    主页布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,95 @@
 <template>
-    <div>
+    <el-container class='home-container'>
+      <!-- 头部区域 -->
+      <el-header>
+        <div>
+          <img src="../assets/heima.png" alt=""/>
+          <span>电商后台管理系统</span>
+        </div>
         <el-button type='info' @click="logout">退出</el-button>
-    </div>
+      </el-header>
+      <!-- 页面主体区域 -->
+      <el-container>
+        <!-- 侧边栏 -->
+        <el-aside width="200px">
+          <!-- 侧边栏菜单 -->
+          <el-menu background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+      <el-submenu index="1">
+        <!-- 一级菜单的模板区域 -->
+        <template slot="title">
+          <!-- 图标 -->
+          <i class="el-icon-location"></i>
+          <!-- 文本 -->
+          <span>导航一</span>
+        </template>
+        <!-- 二级菜单 -->
+        <el-menu-item index="1-4-1">
+          <!-- 图标 -->
+          <i class="el-icon-location"></i>
+          <!-- 文本 -->
+          <span>导航一</span>
+        </el-menu-item>
+      </el-submenu>
+    </el-menu>
+        </el-aside>
+        <!-- 右侧内容主体 -->
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+    <!-- 开始的时候这些组件都没办法用 需要在element.js中导入一下才行 -->
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      // 左侧菜单数据挂载
+      menulist: []
+    }
+  },
+  // 定义生命周期函数
+  created() {
+    this.getMenuList()// 获取所有左侧菜单
+  },
   methods: {
-    logout () {
+    logout() {
       window.sessionStorage.clear()// 清空token
       this.$router.push('/login')// 重新到login界面
+    },
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menulist = res.data
+      console.log(res)
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+  .el-header {
+    background-color: #373d41;
+    display: flex;
+    justify-content: space-between; /* 左右贴边 */
+    padding-left: 0;
+    align-items: center;  /* 垂直居中 */
+    color: #fff;
+    font-size: 20px;
+    > div {
+      display: flex;
+      align-items: center;
+      span{
+        margin-left: 15px;
+      }
+    }
+  }
+  .el-aside {
+    background-color: #333744;
+  }
+  .el-main{
+    background-color: #eaedf1;
+  }
+  .home-container{
+    height: 100%;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import './assets/css/global.css' /* 导入全局样式表 */
 import './assets/fonts/iconfont.css' /* 导入字体图标 */
 import axios from 'axios' /* 挂载请求 */
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/' // 配置请求根路径 每次都和s较劲。。
+// 添加请求拦截器=>预处理过程：通过axios请求拦截器添加token，保证拥有获取数据的权限
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+  // 在最后必须return config
+})
+// 通过use为请求拦截器挂载回调函数 config 请求对象
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
