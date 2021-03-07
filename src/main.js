b7cc49e9100249b0bd26a93f7d048a0293ee6ef8
SpringOnU <760604648@qq.com>
2021-03-07 16:31:25 +0800

    tree-table

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import './plugins/element.js'
 import './assets/css/global.css' /* 导入全局样式表 */
 import './assets/fonts/iconfont.css' /* 导入字体图标 */
 import axios from 'axios' /* 挂载请求 */
+import TreeTable from 'vue-table-with-tree-grid'
+
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/' // 配置请求根路径 每次都和s较劲。。
 // 添加请求拦截器=>预处理过程：通过axios请求拦截器添加token，保证拥有获取数据的权限
 axios.interceptors.request.use(config => {
@@ -17,6 +19,8 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
