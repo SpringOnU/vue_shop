c5eee45abe69fe8b5f154a6dcda2458303fbfdd0
SpringOnU <760604648@qq.com>
2021-03-08 21:14:25 +0800

    tree_table

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -16,7 +16,28 @@
             </el-row>
 
             <!-- 表格 -->
-            <tree-table :data="catelist" :columns="columns"></tree-table>
+            <tree-table :data="catelist" :columns="columns" :selection-type="false" :expand-type="false" :show-index="true" index-text="#" border :show-row-hover="false">
+                <!-- selection-type复选框 expand-type展开行 show-index索引 show-row-hover高亮-->
+
+                <!-- 是否有效 -->
+                <template slot="isok" slot-scope="scope">
+                    <i class="el-icon-success" v-if="scope.row.cat_deleted === false" style="color: lightgreen"></i>
+                    <i class="el-icon-error" v-else style="color: red"></i>
+                </template>
+
+                <!-- 排序 -->
+                <template slot="order" slot-scope="scope">
+                    <el-tag size="mini" v-if="scope.row.cat_level === 0">一级</el-tag>
+                    <el-tag type="success" size="mini" v-else-if="scope.row.cat_level === 1">二级</el-tag>
+                    <el-tag type="warning" size="mini" v-else>三级</el-tag>
+                </template>
+
+                <!-- 操作 -->
+                <template slot="opt"> slot-scope="scope"
+                    <el-button type="primary" icon="el-icon-edit"  size="mini">编辑</el-button>
+                    <el-button type="danger" icon="el-icon-delete"  size="mini"> 删除</el-button>
+                </template>
+            </tree-table>
             <!-- 分页 -->
         </el-card>
     </div>
@@ -32,10 +53,32 @@ export default {
                 pagesize: 3
             },
             // 商品分类的数据列表，默认为空
-            catelist: []
+            catelist: [],
+            // 总数据条数
+            total: 0,
+            // 为table指定列的定义
+            columns: [{
+                label: '分类名称',
+                prop: 'cat_name'
+            },
+            {
+                label: '是否有效',
+                type: 'template', /* 将当前列定义为模板列 */
+                template: 'isok' /* 表示当前这一列使用模板名称 */
+            },
+            {
+                label: '排序',
+                type: 'template', /* 将当前列定义为模板列 */
+                template: 'order' /* 表示当前这一列使用模板名称 */
+            },
+            {
+                label: '操作',
+                type: 'template', /* 将当前列定义为模板列 */
+                template: 'opt' /* 表示当前这一列使用模板名称 */
+            }]
         }
     },
-    create() {
+    created() {
         this.getCateList();
     },
     methods: {
@@ -43,13 +86,13 @@ export default {
             const { data: res } = await this.$http.get('categories',  {
                 params: this.queryInfo
             })
-            console.log(res.data)
             if (res.meta.status !== 200) {
                 return this.$message.error('获取信息失败')
             }
-            console.log(res.data)
+            // console.log(res.data)
             // 把数据列表赋值给cateList
-            this.cateList = res.data.result
+            this.catelist = res.data.result
+            this.total = res.data.total
         }
     }
 }
